ad36ab3dc850cff5b53e52a549a8d8f08fcc3bbf
dedoxd2 <dedo.xd33@yahoo.com>
2024-04-08 16:44:21 +0200

    first 6 vids

---
+++ TestApiJWT/TestApiJWT/Helper/JWT.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApiJWT.Helper
+{
+    public class JWT
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string DurationInDays { get; set; }
+
+        
+
+    }
+}
---
+++ TestApiJWT/TestApiJWT/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace TestApiJWT.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {                                   //IdentityUserContext if we dont have custom user model 
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+    }
+
+}
